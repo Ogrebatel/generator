178bbd8a952a7da5743977c5a4c05aea9b2625f6
Ogrebatel <54839638+Ogrebatel@users.noreply.github.com>
2020-07-08 16:46:06 +0300

    Update math_polynom.c

--- math_polynom.c
+++ math_polynom.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdint.h>
 
-uint16_t remainder_of_division(uint16_t dividend, uint16_t divider){
-    uint16_t tmp_divider;
-    while(dividend >= divider){
-        tmp_divider = divider;
-        while ( ((dividend ^ tmp_divider) >= dividend) || ((dividend ^ tmp_divider) >= tmp_divider) )
-            tmp_divider <<=1;
-        dividend ^= tmp_divider;
+uint16_t remainder_of_division(uint16_t dividend, uint16_t polynom){
+    uint16_t tmp_polynom = polynom;
+    tmp_polynom <<= 7;
+    while(dividend >= polynom){
+        if ((tmp_polynom ^ dividend) <= dividend)
+            dividend ^= tmp_polynom;
+        tmp_polynom >>= 1;
     }
     return dividend;
 }
@@ -23,17 +23,18 @@ int multiplication(uint16_t a, uint16_t b, uint16_t poly){
     return remainder_of_division(x, poly);
 }
 
-int summation(uint16_t a, uint16_t b, uint16_t poly){
+int summation(uint16_t a, uint16_t b, uint16_t polynom){
     return a^b;
 }
 
 int main() {
-    uint16_t a = 0b1010011;
-    uint16_t b = 0b1001;
+    uint16_t a = 83;
+    uint16_t b = 9;
     uint16_t poly = 0b111000011;
 
     printf("summ: %d + %d = %d\n", a, b, summation(a, b, poly));
-    printf("multiplication: %d * %d = %d\n", a, b, multiplication(a, b, poly));
     printf("remainder: %d / %d = %d\n", a, poly, remainder_of_division(a, poly));
+    printf("multiplication: %d * %d = %d\n", a, b, multiplication(a, b, poly));
+
     return 0;
 }
